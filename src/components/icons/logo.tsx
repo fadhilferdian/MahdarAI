bb7c3d52a3b4fada8f6b37d0f1d68c0afd726fad
Fadhil Ferdian <goonersa4@gmail.com>
2025-11-12 13:53:38 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -3,155 +3,79 @@ import type { SVGProps } from 'react';
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="190"
-      height="199"
-      viewBox="0 0 189.61806 198.4375"
-      version="1.1"
-      id="svg1"
-      xmlSpace="preserve"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      {...props}
-    >
-      <g
-        id="layer1"
-        transform="translate(-20.381945,-38.340149)"
-        fill="currentColor"
-      >
-        <g
-          style={{ fill: 'none' }}
-          id="g1"
-          transform="matrix(8.8194444,0,0,8.8194444,0.5381955,27.315844)"
-        >
-          <path
-            d="M 8,2 V 5"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeMiterlimit="10"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            id="path1"
-            style={{ fill: 'hsl(var(--primary))' }}
-          />
-          <path
-            d="M 16,2 V 5"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeMiterlimit="10"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            id="path2"
-            style={{ fill: 'hsl(var(--primary))' }}
-          />
-          <path
-            d="m 21,8.5 v 5.13 c -0.89,-0.71 -2.02,-1.13 -3.25,-1.13 -1.23,0 -2.38,0.43 -3.28,1.16 -1.21,0.95 -1.97,2.44 -1.97,4.09 0,0.98 0.28,1.92 0.76,2.7 0.37,0.61 0.85,1.14 1.42,1.55 H 8 C 4.5,22 3,20 3,17 V 8.5 c 0,-3 1.5,-5 5,-5 h 8 c 3.5,0 5,2 5,5 z"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeMiterlimit="10"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            id="path3"
-            style={{ fill: 'hsl(var(--primary))' }}
-          />
-          <path
-            d="m 23,17.75 c 0,0.98 -0.28,1.92 -0.76,2.7 -0.28,0.48 -0.63,0.9 -1.04,1.24 -0.92,0.82 -2.12,1.31 -3.45,1.31 -1.15,0 -2.21,-0.37 -3.07,-1 -0.57,-0.41 -1.05,-0.94 -1.42,-1.55 -0.48,-0.78 -0.76,-1.72 -0.76,-2.7 0,-1.65 0.76,-3.14 1.97,-4.09 0.9,-0.73 2.05,-1.16 3.28,-1.16 1.23,0 2.36,0.42 3.25,1.13 1.22,0.96 2,2.45 2,4.12 z"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeMiterlimit="10"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            id="path6"
-            style={{ fill: 'hsl(var(--primary))' }}
-          />
-          <path
-            opacity="0.4"
-            d="m 17.75,20.25 c 0,-1.38 1.12,-2.5 2.5,-2.5 -1.38,0 -2.5,-1.12 -2.5,-2.5 0,1.38 -1.12,2.5 -2.5,2.5 1.38,0 2.5,1.12 2.5,2.5 z"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            id="path7"
-            style={{ fill: 'hsl(var(--primary))' }}
-          />
-          <text
-            xmlSpace="preserve"
-            style={{
-              fontWeight: 500,
-              fontSize: '5.29632px',
-              lineHeight: 1,
-              fontFamily: 'Amiri',
-              textAlign: 'start',
-              letterSpacing: '0.0529632px',
-              wordSpacing: '1.05926px',
-              writingMode: 'lr-tb',
-              direction: 'ltr',
-              textAnchor: 'start',
-              fill: '#000000',
-              strokeWidth: 1.74153,
-            }}
+    width="716.66669"
+    height="750"
+    viewBox="0 0 189.61806 198.4375"
+    version="1.1"
+    id="svg1"
+    xmlSpace="preserve"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+    ><defs
+      id="defs1" /><g
+      id="layer1"
+      transform="translate(-20.381945,-38.340149)"><g
+        style={{fill:'none'}}
+        id="g1"
+        transform="matrix(8.8194444,0,0,8.8194444,0.5381955,27.315844)"><path
+          d="M 8,2 V 5"
+          stroke="#292d32"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          id="path1"
+          style={{fill:'#87cdde'}} /><path
+          d="M 16,2 V 5"
+          stroke="#292d32"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          id="path2"
+          style={{fill:'#87cdde'}} /><path
+          d="m 21,8.5 v 5.13 c -0.89,-0.71 -2.02,-1.13 -3.25,-1.13 -1.23,0 -2.38,0.43 -3.28,1.16 -1.21,0.95 -1.97,2.44 -1.97,4.09 0,0.98 0.28,1.92 0.76,2.7 0.37,0.61 0.85,1.14 1.42,1.55 H 8 C 4.5,22 3,20 3,17 V 8.5 c 0,-3 1.5,-5 5,-5 h 8 c 3.5,0 5,2 5,5 z"
+          stroke="#292d32"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          id="path3"
+          style={{fill:'#87cdde'}} /><path
+          d="m 23,17.75 c 0,0.98 -0.28,1.92 -0.76,2.7 -0.28,0.48 -0.63,0.9 -1.04,1.24 -0.92,0.82 -2.12,1.31 -3.45,1.31 -1.15,0 -2.21,-0.37 -3.07,-1 -0.57,-0.41 -1.05,-0.94 -1.42,-1.55 -0.48,-0.78 -0.76,-1.72 -0.76,-2.7 0,-1.65 0.76,-3.14 1.97,-4.09 0.9,-0.73 2.05,-1.16 3.28,-1.16 1.23,0 2.36,0.42 3.25,1.13 1.22,0.96 2,2.45 2,4.12 z"
+          stroke="#292d32"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          id="path6"
+          style={{fill:'#87cdde'}} /><path
+          opacity="0.4"
+          d="m 17.75,20.25 c 0,-1.38 1.12,-2.5 2.5,-2.5 -1.38,0 -2.5,-1.12 -2.5,-2.5 0,1.38 -1.12,2.5 -2.5,2.5 1.38,0 2.5,1.12 2.5,2.5 z"
+          stroke="#292d32"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          id="path7"
+          style={{fill:'#87cdde'}} /><text
+          xmlSpace="preserve"
+          style={{fontWeight:500,fontSize:'5.29632px',lineHeight:1,fontFamily:'Gotham Black',WebkitFontSpecification:'Gotham Black,  Medium',textAlign:'start',letterSpacing:'0.0529632px',wordSpacing:'1.05926px',writingMode:'lr-tb',direction:'ltr',textAnchor:'start',fill:'#000000',strokeWidth:'1.74153'}}
+          x="5.5296435"
+          y="10.534536"
+          id="text1"><tspan
+            id="tspan1"
+            style={{fontStyle:'normal',fontVariant:'normal',fontWeight:500,fontStretch:'normal',fontFamily:'DG Baysan',WebkitFontSpecification:'DG Baysan Medium',letterSpacing:'0.0529632px',fill:'#000000',strokeWidth:'1.74153'}}
             x="5.5296435"
-            y="10.534536"
-            id="text1"
-          >
-            <tspan
-              id="tspan1"
-              style={{
-                fontStyle: 'normal',
-                fontVariant: 'normal',
-                fontWeight: 500,
-                fontStretch: 'normal',
-                fontFamily: 'Amiri',
-                letterSpacing: '0.0529632px',
-                fill: '#000000',
-                strokeWidth: 1.74153,
-              }}
-              x="5.5296435"
-              y="10.534536"
-            >
-              محضر
-            </tspan>
-          </text>
-          <text
-            xmlSpace="preserve"
-            style={{
-              fontStyle: 'normal',
-              fontVariant: 'normal',
-              fontWeight: 500,
-              fontStretch: 'normal',
-              fontSize: '4.79504px',
-              lineHeight: 1,
-              fontFamily: 'Inter',
-              textAlign: 'start',
-              letterSpacing: '0.0479504px',
-              wordSpacing: '0.959008px',
-              writingMode: 'lr-tb',
-              direction: 'ltr',
-              textAnchor: 'start',
-              fill: '#000000',
-              strokeWidth: 1.5767,
-            }}
+            y="10.534536">محضر</tspan></text><text
+          xmlSpace="preserve"
+          style={{fontStyle:'normal',fontVariant:'normal',fontWeight:500,fontStretch:'normal',fontSize:'4.79504px',lineHeight:1,fontFamily:'DG Baysan',WebkitFontSpecification:'DG Baysan Medium',textAlign:'start',letterSpacing:'0.0479504px',wordSpacing:'0.959008px',writingMode:'lr-tb',direction:'ltr',textAnchor:'start',fill:'#000000',strokeWidth:'1.5767'}}
+          x="5.2681246"
+          y="18.12989"
+          id="text2"><tspan
+            id="tspan2"
+            style={{fontStyle:'normal',fontVariant:'normal',fontWeight:'normal',fontStretch:'normal',fontFamily:'Gotham Black',WebkitFontSpecification:'Gotham Black, ',strokeWidth:'1.5767'}}
             x="5.2681246"
-            y="18.12989"
-            id="text2"
-          >
-            <tspan
-              id="tspan2"
-              style={{
-                fontStyle: 'normal',
-                fontVariant: 'normal',
-                fontWeight: 'bold',
-                fontStretch: 'normal',
-                fontFamily: 'Inter',
-                strokeWidth: 1.5767,
-              }}
-              x="5.2681246"
-              y="18.12989"
-            >
-              AI
-            </tspan>
-          </text>
-        </g>
-      </g>
-    </svg>
+            y="18.12989">AI</tspan></text></g></g>
+            </svg>
   );
 }
