2e17c8e408b05c2023b75925bd20cc00cc5907be
Fadhil Ferdian <goonersa4@gmail.com>
2025-11-26 05:00:18 +0000

    kembalikan ke justify between

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-center px-4 md:px-6">
+      <div className="container flex h-16 items-center justify-between px-4 md:px-6">
         <Link href="/" className="flex items-center space-x-2">
           <Logo className="h-10 w-10 text-primary-foreground" />
           <span className="font-headline text-xl font-bold tracking-tight">
@@ -22,7 +22,7 @@ export default function Header() {
         </Link>
         
         {!isSummarizerPage && (
-          <div className="flex items-center justify-end space-x-4 ml-8">
+          <div className="flex items-center justify-end space-x-4">
             <Button asChild>
               <Link href="/summarizer">
                 Coba Sekarang <ArrowRight className="ml-2 h-4 w-4" />
